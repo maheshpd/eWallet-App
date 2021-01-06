5061edd655f93969fa75f7880e528370de1a16d1
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-06 16:15:25 +0530

    Initial Commit

---
+++ eWallet App/eWallet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eWallet_AppApp.swift
+//  eWallet App
+//
+//  Created by Mahesh Prasad on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct eWallet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
